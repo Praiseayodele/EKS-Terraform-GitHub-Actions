705ea89c0a33325c3604c72efb871c3a2cdd6334
Praiseayodele <118330535+Praiseayodele@users.noreply.github.com>
2026-01-26 07:06:40 -0600

    Update eks-access.tf

--- module/eks-access.tf
+++ module/eks-access.tf
@@ -1,6 +1,17 @@
 resource "aws_eks_access_entry" "jump_server" {
-  cluster_name = aws_eks_cluster.eks[0].name
+  cluster_name  = aws_eks_cluster.eks[0].name
   principal_arn = "arn:aws:iam::163205449102:role/Mernstackproject"
 
   type = "STANDARD"
+}
+
+resource "aws_eks_access_policy_association" "jump_server_admin" {
+  cluster_name  = aws_eks_cluster.eks[0].name
+  principal_arn = aws_eks_access_entry.jump_server.principal_arn
+
+  policy_arn = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
+
+  access_scope {
+    type = "cluster"
+  }
 }

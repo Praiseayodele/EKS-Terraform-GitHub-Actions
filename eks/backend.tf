a20fa61bf3a46b5c24760e205f522a1801083642
Praiseayodele <118330535+Praiseayodele@users.noreply.github.com>
2026-03-12 17:52:40 -0500

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -5,14 +5,13 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.49.0"
     }
-  }
-  backend "s3" {
-    bucket         = "praises-terraform-state-bucket"
+backend "s3" {
+    bucket         = "praise-tf-state-161807613169"
     region         = "us-east-1"
     key            = "eks/terraform.tfstate"
     dynamodb_table = "terraform-locks"
     encrypt        = true
-  }
+}
 }
 
 provider "aws" {

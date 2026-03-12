4f536bfd307d49171a49c413f9efaf684eaeffc8
Praiseayodele <118330535+Praiseayodele@users.noreply.github.com>
2026-03-12 17:48:38 -0500

    Update backend.tf

--- eks/backend.tf
+++ eks/backend.tf
@@ -8,7 +8,7 @@ terraform {
   }
   backend "s3" {
     bucket         = "praises-terraform-state-bucket"
-    region         = "us-east-2"
+    region         = "us-east-1"
     key            = "eks/terraform.tfstate"
     dynamodb_table = "terraform-locks"
     encrypt        = true
